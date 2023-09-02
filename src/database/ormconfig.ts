3e558113d7550e22e3fa11348d75e2bb559475aa
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2023-09-02 13:10:31 -0300

    Add files via upload

---
+++ src/database/ormconfig.ts
@@ -0,0 +1,28 @@
+import {DataSource} from 'typeorm'
+import dotenv from 'dotenv'
+import { join } from 'path';
+
+//le o arquivo
+dotenv.config();
+
+//criando as configurações do banco de dados
+const dataBase = new DataSource({
+    type:'sqlite',
+    database:process.env.DATABASE || './db.sqlite',
+    entities: [
+        //'.src/models/*.ts'
+        join(__dirname, '..', 'models/*.{ts,js}')
+    ],
+    logging: true,  //toda vez que executar uma operação no banco de dados ele escreve no console pra verificar o comando executado
+    synchronize: true  //quando cria uma nova entidade ele apaga tudo e refaz ela automaticamente
+})
+
+//devolve uma promessa ok/falha/fica rodando
+dataBase.initialize()
+.then(() => {
+    console.log('Banco conectado com sucesso')
+}).catch((erro)=>{
+    console.log('Erro ao conectar no banco! ',erro)
+})
+
+export default dataBase
